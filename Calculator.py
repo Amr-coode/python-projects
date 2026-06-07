3d8e20ca608e5d4d6a8318713723e39378b9409f
Amr-codes <maheramr656@gmail.com>
2026-06-08 02:56:57 +0300

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,14 @@
+num1 = float(input("Frist Number : "))
+num2 = float(input("Second Number : "))
+op = input("Select The Operation  + - * / : ")
+   
+if op == "+":
+       print("Output:", num1 + num2)
+elif op == "-":
+       print("Output:", num1 - num2)
+elif op == "*":
+       print("Output:", num1 * num2)
+elif op == "/":
+       print("Output:", num1 / num2)
+else:
+       print("error ")
